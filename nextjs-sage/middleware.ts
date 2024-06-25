106e56aafa0e7a0c84cb769f4d6ed65bb775e2c9
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-25 17:35:45 +1200

    remove custom check

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,9 +3,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     console.log(request.cookies)
   const currentUser = request.cookies.get('currentUser')?.value
-  if (request.nextUrl.pathname.startsWith('/.auth')) {
-    return Response.redirect(request.url)
-  }
+
  
   if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
     return Response.redirect(new URL('/dashboard', request.url))
