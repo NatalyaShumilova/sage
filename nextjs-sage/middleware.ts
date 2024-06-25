00cc6d774490bb39547cb0d5d7dec38915983ac5
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-25 18:18:07 +1200

    currentuser

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -2,6 +2,8 @@ import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
     console.log(request.cookies)
-  
+  const currentUser = request.cookies.get('currentUser')?.value
+
+ 
 }
  
