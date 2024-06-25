abf7004a8c28e9a943b9e729bff4a7704cd02fb5
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-25 21:08:27 +1200

    return default url

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,5 +3,5 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/dashboard'))
+  return NextResponse.redirect(new URL('/home', request.url))
 }
