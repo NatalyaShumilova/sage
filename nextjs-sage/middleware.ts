c0746170f0c93f3bd82d1868e9d6c0d22182033e
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-25 16:05:31 +1200

    Empty middleware

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -1,21 +1,6 @@
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-    console.log(request.cookies)
-  const currentUser = request.cookies.get('currentUser')?.value
-  if (request.nextUrl.pathname.startsWith('/.auth')) {
-    return Response.redirect(request.url)
-  }
- 
-  if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
-    return Response.redirect(new URL('/dashboard', request.url))
-  }
- 
-  if (!currentUser && request.nextUrl.pathname !== '/') {
-    return Response.redirect(new URL('/login', request.url))
-  }
+    
 }
  
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
