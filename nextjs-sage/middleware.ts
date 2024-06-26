8401b153531cb03460a9f796ea3b7a960b7d592e
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-27 11:53:39 +1200

    redirect to login

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,10 +3,7 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get('currentUser')?.value
-  console.log(currentUser)
-  console.log(request.cookies);
-  return NextResponse.redirect(new URL('/dashboard', request.url))
+  return NextResponse.redirect(new URL('/login', request.url))
 }
  
 // See "Matching Paths" below to learn more
