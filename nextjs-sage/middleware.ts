fa79a82f74f838f03e08f072e5d38da563bb4ac2
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-27 11:28:11 +1200

    current paths

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,19 +3,10 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/dashboard', request.url))
+  return NextResponse.redirect(new URL('/login', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/', '/dashboard'],
 }
