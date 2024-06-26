5ab9c87ab8f9b440768d0a3e32958379b514fb7b
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-06-27 11:36:48 +1200

    log request cookies

--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,6 +3,9 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('currentUser')?.value
+  console.log(currentUser)
+  console.log(request.cookies);
   return NextResponse.redirect(new URL('/login', request.url))
 }
  
