d77511174c64c8db78c001d67e6c8178b403659f
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-06 17:19:39 +1200

    Test middleware.ts

--- nextjs-sage/__tests__/middleware.test.ts
+++ nextjs-sage/__tests__/middleware.test.ts
@@ -1,12 +1,25 @@
+/**
+ * @jest-environment node
+ */
+
 import '@testing-library/jest-dom'
 import {middleware} from '../middleware'
 import { NextRequest } from 'next/server'
 import { NextURL } from 'next/dist/server/web/next-url'
+import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 
 describe('middleware', () => {
   it('redirects to login given no cookie', () => {
-    const request = {nextUrl: new NextURL("/dashboard") } as NextRequest;
+    const request = {url: new URL ("http://localhost"), nextUrl: {pathname: "/dashboard"} as NextURL, cookies: {get: jest.fn()}as unknown as RequestCookies} as unknown as NextRequest;
     const response = middleware(request);
-    expect(response.redirected).toBeTruthy();
+    expect(response.status).toBe(307);
+    expect(response.headers.get("location")).toBe("http://localhost/")
+  })
+
+  it('redirects to dashboard given cookie', () => {
+    const request = {url: new URL ("http://localhost"), nextUrl: {pathname: "/"} as NextURL, cookies: {get: jest.fn(() => ({value: "user"}))}as unknown as RequestCookies} as unknown as NextRequest;
+    const response = middleware(request);
+    expect(response.status).toBe(307);
+    expect(response.headers.get("location")).toBe("http://localhost/dashboard")
   })
 })
--- nextjs-sage/middleware.ts
+++ nextjs-sage/middleware.ts
@@ -3,12 +3,12 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log(request)
   const currentUser = request.cookies.get('StaticWebAppsAuthCookie')?.value
   if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
 
   }
+
   if(!currentUser && request.nextUrl.pathname !== '/') {
     return NextResponse.redirect(new URL('/', request.url))
   }
