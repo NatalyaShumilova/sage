f3ec320d4da63885b283ca72a93608b1f258e878
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-10 08:52:13 +1200

    import styled-components

--- nextjs-sage/components/FloatingBox.tsx
+++ nextjs-sage/components/FloatingBox.tsx
@@ -1,13 +1,17 @@
 import React, { ReactNode } from "react"
+import styled from "styled-components"
+
+const StyledDiv = styled.div`
+    background: "white"; 
+    borderRadius: 5px;
+    boxShadow: 1px 4px 8px 0 #89BF8F33;
+    padding: 10px 20px;
+    `;
 
 export const FloatingBox: React.FC<{children: ReactNode}> = ({children}) => {
     return (
-        <div style={{
-        background: "white", 
-        borderRadius: "5px", 
-        boxShadow: "1px 4px 8px 0 #89BF8F33", 
-        padding: "10px 20px"}}>
+        <StyledDiv>
             {children}
-        </div>
+        </StyledDiv>  
     )
 }
