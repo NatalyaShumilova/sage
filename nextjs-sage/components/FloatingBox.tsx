1537e90aaf5b4942671aafa8c55adcd25694c1ac
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-09 22:39:57 +1200

    remove style tags

--- nextjs-sage/components/FloatingBox.tsx
+++ nextjs-sage/components/FloatingBox.tsx
@@ -6,7 +6,7 @@ export const FloatingBox: React.FC<{children: ReactNode}> = ({children}) => {
         background: "white", 
         borderRadius: "5px", 
         boxShadow: "1px 4px 8px 0 #89BF8F33", 
-        padding: "10px"}}>
+        padding: "10px 20px"}}>
             {children}
         </div>
     )
