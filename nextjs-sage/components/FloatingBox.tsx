a6194a7fa5ff066a83de573282db6b5b778f6260
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-10 22:35:05 +1200

    switch background to hexcode

--- nextjs-sage/components/FloatingBox.tsx
+++ nextjs-sage/components/FloatingBox.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react"
 import styled from "styled-components"
 
 const StyledDiv = styled.div`
-    background: "white"; 
+    background: #ffffff; 
     borderRadius: 5px;
     boxShadow: 1px 4px 8px 0 #89BF8F33;
     padding: 10px 20px;
