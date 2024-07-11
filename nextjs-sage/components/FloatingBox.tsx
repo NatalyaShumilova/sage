02aaa23507c82fb449282ecb55df6f0c35b45d3a
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-11 22:41:12 +1200

    switch theme to module

--- nextjs-sage/components/FloatingBox.tsx
+++ nextjs-sage/components/FloatingBox.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import styled from "styled-components"
-import theme from '../styles/theme.scss'
+import theme from '../styles/theme.module.scss'
 
 
 const StyledDiv = styled.div`
