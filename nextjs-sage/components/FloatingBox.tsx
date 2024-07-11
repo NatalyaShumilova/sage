885c969369b8f5a503a8f4fb82b27c4f63517bbb
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-11 23:06:18 +1200

    use global theme in component

--- nextjs-sage/components/FloatingBox.tsx
+++ nextjs-sage/components/FloatingBox.tsx
@@ -4,14 +4,13 @@ import theme from '../styles/theme.module.scss'
 
 
 const StyledDiv = styled.div`
-    background: #ffffff; 
-    border-radius: 5px;
-    box-shadow: 1px 4px 8px 0 #89BF8F33;
+    background: ${theme.light}; 
+    border-radius: ${theme.radius};
+    box-shadow: ${theme.shadow};
     padding: 10px 20px;
     `;
 
 export const FloatingBox: React.FC<{children: ReactNode}> = ({children}) => {
-    console.log(theme)
     return (
         <StyledDiv>
             {children}
