656a5c120d2ba2efa33c0c575146a0ede5b33344
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-18 19:58:16 +1200

    import styles

--- nextjs-sage/components/Navigation.tsx
+++ nextjs-sage/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import '../styles/navigation.module.scss'
+import styles from '../styles/navigation.module.scss'
 import theme from '../styles/theme.module.scss'
 import { HomeIcon} from './HomeIcon'
 
@@ -12,27 +12,25 @@ interface NavigationProps {
 export const Navigation: React.FC<NavigationProps> = ({current, setCurrent, screens}) => {
     const index = screens.indexOf(current);
     return (
-        <>
-            <div className="navContainer">
+            <div className={styles.navContainer}>
                 <div>
                     <div 
                     onClick={() => setCurrent(screens[(index+2)%3])} 
-                    className={screens[(index+2)%3] === "Home" ? "homeIndicator" : "indicator"}
+                    className={screens[(index+2)%3] === "Home" ? styles.homeIndicator : styles.indicator}
                     >
                         {screens[(index+2)%3] === "Home" && <HomeIcon color={theme.purple75}/>}
                     </div>
-                    <div className={current === "Home" ? "homeIndicator" : "indicator current"}>
+                    <div className={current === "Home" ? styles.homeIndicator : (styles.indicator +" "+ styles.current)}>
                         {current === "Home" && <HomeIcon color={theme.purple}/>}
                     </div>
                     <div 
                     onClick={() => setCurrent(screens[(index+1)%3])} 
-                    className={screens[(index+1)%3] === "Home" ? "homeIndicator" : "indicator"}
+                    className={screens[(index+1)%3] === "Home" ? styles.homeIndicator : styles.indicator}
                     >
                         {screens[(index+1)%3] === "Home" && <HomeIcon color={theme.purple75}/>}
                     </div>
                 </div>
                 {current}
             </div>
-        </>
     )
 }
