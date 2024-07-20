201d897915a4e15dccab9f3f7b667b51a145448a
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-20 15:48:40 +1200

    AddFood adjustments

--- nextjs-sage/components/AddFood.tsx
+++ nextjs-sage/components/AddFood.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/addfood.module.scss'
 export const AddFood = () => {
     return (
         <div className={styles.addFood} onClick={() => console.log('call assembly!')}>
-            <div/>
+            <div>
+                <img src="/Add.png"/>
+            </div>
             <p>Add Food</p>
         </div>
     )
