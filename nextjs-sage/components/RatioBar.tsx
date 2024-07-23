748a5a538167868f761126a8307b60ebf5b8a962
NatalyaShumilova <natalya.shumilova@gmail.com>
2024-07-23 13:30:25 +1200

    RatioBar Component

---
+++ nextjs-sage/components/RatioBar.tsx
@@ -0,0 +1,45 @@
+import styles from '../styles/ratiobar.module.scss'
+
+const getWidth = (current: number, idealMax: number, idealMin: number) => {
+    if (current > idealMax) {
+        const percentage = (current - idealMax) / idealMax 
+        return `${50 + Math.round(percentage*50)}%`
+    }
+    if (current < idealMin) {
+        const percentage = (current - idealMin/2) / (idealMin/2) 
+        return `${Math.round(percentage*50)}%`
+    }
+    return "50%";
+    
+}
+
+interface RatioBarProps {
+    ratio: {
+        min: number;
+        max: number
+        item1: {
+            name: string;
+            color: string;
+        }
+        item2: {
+            name: string;
+            color: string;
+        }
+    };
+    current: number;
+}
+
+export const RatioBar: React.FC<RatioBarProps> = ({ratio, current}) => {
+
+    return (
+        <div>
+            <div className={styles.bar}>
+                <div data-testid="bar" style={{width: getWidth(current, ratio.max, ratio.min)}}/>
+            </div>
+            <div className={styles.labels}>
+                <p>{ratio.item1.name}</p>
+                <p>{ratio.item2.name}</p>
+            </div>
+        </div>
+    )
+}
---
+++ nextjs-sage/components/__tests__/RatioBar.test.tsx
@@ -0,0 +1,49 @@
+import { act, render, waitFor, screen } from "@testing-library/react"
+import '@testing-library/jest-dom';
+import { RatioBar } from "../RatioBar"
+ 
+const mockRatio = {
+    item1: {
+        name: "Sodium",
+        color: "#ffffff"
+    },
+    item2: {
+        name: "Potassium",
+        color: "#000000"
+    },
+    min: 0.6,
+    max: 0.8
+}
+
+describe("RatioBar", () => {
+    it("returns correct width given ratio within range", async () => {
+        act(() => {
+            render(<RatioBar ratio={mockRatio} current={0.7} />)
+        })
+
+        await waitFor(() => {});
+        const bar = screen.getByTestId("bar");
+        console.log(expect(bar))
+        expect(screen.getByTestId("bar")).toHaveStyle('width: 50%');
+    })
+    it("returns correct width given ratio below range", async () => {
+        act(() => {
+            render(<RatioBar ratio={mockRatio} current={0.45} />)
+        })
+
+        await waitFor(() => {});
+        const bar = screen.getByTestId("bar");
+        console.log(expect(bar))
+        expect(screen.getByTestId("bar")).toHaveStyle('width: 25%');
+    })
+    it("returns correct width given ratio above range", async () => {
+        act(() => {
+            render(<RatioBar ratio={mockRatio} current={1.2} />)
+        })
+
+        await waitFor(() => {});
+        const bar = screen.getByTestId("bar");
+        console.log(expect(bar))
+        expect(screen.getByTestId("bar")).toHaveStyle('width: 75%');
+    })
+})
